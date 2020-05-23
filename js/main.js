6bb955128814a28bf8522e921e0c74166382101f
Joan_E <joanevans18@gmail.com>
2020-05-23 14:46:33 +0300

    Added a hide/show effect on delivery location div

--- js/main.js
+++ js/main.js
@@ -1,16 +1,31 @@
+$(document).ready(function(e){
+    
+    $('#location').hide();
+   
+    $("input[name='deliveryOptions']").change(function(){      
+        var inputValue = Boolean($(this).attr("value"));
+        if(inputValue) {
+            $('#location').show();
+            // if($('#location').val() == ''){
 
+            // }          
+        }else{
+            $('#location').hide();
+        }   
+    });
+    
 
-
-$(document).ready(function(){
     $('#orderForm').submit(function(e){
         e.preventDefault();
         var name = $('#name').val();
         var pizza = $('#pizza').val();
         var selectedCrust = $("input[name='crustType']:checked").val();
         var selectedToppings = [];
+        var deliveryAddress = $('#location').val();
+
         $.each($("input[name='topping']:checked"),function(){
             selectedToppings.push($(this).val())
         });
-        console.log(`${name}, ${pizza}, ${selectedCrust},  ${ selectedToppings.length} `)
+        console.log(add)
     });
 });
