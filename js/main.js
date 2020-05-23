d591071cb7279400de0ccce5bb2962318387db6b
Joan_E <joanevans18@gmail.com>
2020-05-23 22:09:32 +0300

    Added a constructor function(Order) and business logic

--- js/main.js
+++ js/main.js
@@ -1,9 +1,69 @@
 // Business Logic
+function Order(name,size,crust,toppings, delivery){
+    this.name = name;
+    this.pizzaSize = size;
+    this.pizzaCrust = crust;
+    this.pizzaToppings = toppings;    
+    this.delivery = delivery;
+    this.price = 0;
+
+}
+Order.prototype.determinePrice = function(){	
+    this.price += pizzaSizePrice(this.pizzaSize);
+    this.price += pizzaCrustprice(this.pizzaCrust);
+    this.pizzaToppings.forEach((topping) => {
+    	this.price += pizzaToppingsPrice(topping);
+    });
+    return this.price
+}
+Order.prototype.determinePricePlusDelivery = function(){
+    this.price += pizzaSizePrice(this.pizzaSize);
+    this.price += pizzaCrustprice(this.pizzaCrust);
+    this.pizzaToppings.forEach((topping) => {
+    	this.price += pizzaToppingsPrice(topping);
+    });
+    this.price += 100
+    return this.price
+}
+
+
+function pizzaSizePrice(size){
+    if (size == 'Big'){
+  	    return 500;
+    } else if(size == 'Medium'){
+  	    return 300;
+    } else{
+  	    return 250;
+    }
+};
+function pizzaCrustprice(crust){
+	if (crust == 'glutenFree'){
+  	    return 50
+    } else if(crust == 'crusty'){
+  	    return 35
+    } else{
+  	    return 25
+    }
+};
+
+function pizzaToppingsPrice(topping){
+	if (topping == 'mushrooms'){
+  	    return 0
+    }
+    if (topping == 'pepperoni'){
+  	    return 15
+    }
+    if (topping == 'beef'){
+  	    return 5
+    }
+    if (topping == 'blackOlives'){
+  	    return 20
+    }
+};
 
 $(document).ready(function(){
     // UI Logic
     // Validations
-
     // Name and Size validations
     $('.sizeError').hide();
     $('.nameError').hide();
@@ -29,12 +89,11 @@ $(document).ready(function(){
 
     // Delivery section validation
     $('#locationDiv').hide();
-    $('.lctError').hide();
-      
+    $('.lctError').hide();      
    
-    $("input[name='deliveryOptions']").change(function(){      
-        var inputValue = Boolean($(this).attr("value"));
-        if(inputValue) {
+    $('input[name="deliveryOptions"]').click(function(){      
+        var inputValue = $(this).attr('value');
+        if(inputValue == 'true') {
             $('#locationDiv').show();           
             $('#location').focusout(function() {
                  console.log($(this).val());
@@ -47,7 +106,7 @@ $(document).ready(function(){
                 }
             });                      
         }else{
-             $('#location').hide();
+            $('#locationDiv').hide();
         }   
     });
     
@@ -58,11 +117,22 @@ $(document).ready(function(){
         var pizza = $('#pizza').val();
         var selectedCrust = $("input[name='crustType']:checked").val();
         var selectedToppings = [];
+        var deliveryOption = $("input[name='deliveryOptions']:checked").val();       
         var deliveryAddress = $('#location').val();
+        var orders = []
 
         $.each($("input[name='topping']:checked"),function(){
             selectedToppings.push($(this).val())
         });
-        console.log(add)
+
+        var newOrder = new Order(name, pizza, selectedCrust, selectedToppings, deliveryOption)
+        console.log(newOrder)
+        if(newOrder[deliveryOption] == true){
+            console.log(newOrder.determinePricePlusDelivery());
+        }else {
+            newOrder.determinePrice();
+
+        }
+        
     });
 });
