c7a1559e36fead06367935b9e4224cbcec13a7ec
Joan_E <joanevans18@gmail.com>
2020-05-23 17:24:02 +0300

    Added UI logic(Name, PizzaSize and delivery location validations)

--- js/main.js
+++ js/main.js
@@ -1,16 +1,53 @@
-$(document).ready(function(e){
-    
-    $('#location').hide();
+// Business Logic
+
+$(document).ready(function(){
+    // UI Logic
+    // Validations
+
+    // Name and Size validations
+    $('.sizeError').hide();
+    $('.nameError').hide();
+    $('#name').focusout(function() {
+        if($(this).val() == ''){
+            $('.nameError').show();    
+            $(this).addClass("is-invalid")
+
+        }else{
+            $('.nameError').hide();    
+            $(this).removeClass("is-invalid")
+        }
+    });
+    $('#pizza').focusout(function() {
+        if($(this).val() == '-Select--'){
+            $('.sizeError').show();
+            $(this).addClass("is-invalid");
+        }else{
+            $('.sizeError').hide();
+            $(this).removeClass("is-invalid");
+        }
+    });
+
+    // Delivery section validation
+    $('#locationDiv').hide();
+    $('.lctError').hide();
+      
    
     $("input[name='deliveryOptions']").change(function(){      
         var inputValue = Boolean($(this).attr("value"));
         if(inputValue) {
-            $('#location').show();
-            // if($('#location').val() == ''){
-
-            // }          
+            $('#locationDiv').show();           
+            $('#location').focusout(function() {
+                 console.log($(this).val());
+                if($('#location').val() === ''){
+                    $('#location').addClass('is-invalid');            
+                    $('.lctError').show();
+                }else{
+                    $('.lctError').hide();
+                    $('#location').removeClass('is-invalid');          
+                }
+            });                      
         }else{
-            $('#location').hide();
+             $('#location').hide();
         }   
     });
     
